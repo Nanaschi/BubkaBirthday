bb64b1c0f694e89270bb14f32b2474e3aa5af923
Dani <f1gasl74r1@gmail.com>
2022-06-12 16:49:15 +0300

    Extra fixes

--- Assets/_Scripts/ZenjectInstallers/SceneController.cs
+++ Assets/_Scripts/ZenjectInstallers/SceneController.cs
@@ -12,11 +12,15 @@ public class SceneController: MonoBehaviour
     private void OnEnable()
     {
         _uiController.Start.onClick.AddListener(LoadNextLevel);
+        _uiController.Exit.onClick.AddListener(ExitApplication);
     }
+
+
 
     private void OnDisable()
     {
         _uiController.Start.onClick.RemoveListener(LoadNextLevel);
+        _uiController.Exit.onClick.RemoveListener(ExitApplication);
     }
 
     #endregion
@@ -25,5 +29,9 @@ public class SceneController: MonoBehaviour
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    private void ExitApplication()
+    {
+        Application.Quit();
     }
 }
